06abd2163fd87db63826c66c9033f4542284df04
Navdeep Jajoria <106376368+navdeep545@users.noreply.github.com>
2026-05-01 11:26:27 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+
+void dfs(string curr , int open, int closed, unordered_set<string> &ans){
+    if(open == 0 and closed == 0){
+        ans.insert(curr) ;
+        return ;
+    } 
+    if(open > 0) dfs(curr + "(", open-1, closed, ans) ;
+    if(open == closed){
+        dfs(curr + "(", open - 1, closed, ans) ;
+        return ;
+    }
+    if(closed > open){
+        dfs(curr + ")", open, closed - 1, ans) ;
+    }
+    return ;
+}
+
+public:
+    vector<string> generateParenthesis(int n) {
+        unordered_set<string> ans ;
+        string curr = "" ;
+        dfs(curr,n,n, ans) ;
+        return vector<string> (ans.begin(), ans.end()) ;
+    }
+};
